e0603c5b7c7dcd8c8609297b8775a509f1035506
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-03-11 19:41:11 +0100

    add card partial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,7 @@
 <div class="row">
             @foreach ($movies as $movie)
         <div class="col-md-4">
-            <div class="card mb-4">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text">{{ $movie->original_title }}</p>
-                    <p class="card-text">{{ $movie->nationality }}</p>
-                    <p class="card-text"><strong>Date:</strong> {{ $movie->date }}</p>
-                    <p class="card-text"><strong>Vote:</strong> {{ $movie->vote }}</p>
-
-                    
-
-                </div>
-            </div>
+           @include('partials.film-card')
         </div>
         @endforeach
     </div>
---
+++ resources/views/partials/film-card.blade.php
@@ -0,0 +1,12 @@
+<div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <p class="card-text">{{ $movie->original_title }}</p>
+                    <p class="card-text">{{ $movie->nationality }}</p>
+                    <p class="card-text"><strong>Date:</strong> {{ $movie->date }}</p>
+                    <p class="card-text"><strong>Vote:</strong> {{ $movie->vote }}</p>
+
+                    
+
+                </div>
+            </div>
